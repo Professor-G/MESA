7342dc54b0a12ceb4fdbad586aadbedc7de57938
Dylan Gatlin <dgatlin5@gmail.com>
2022-04-05 18:02:45 -0600

    Init push

---
+++ build_inlists.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import numpy as np
+from pathlib import Path
+
+here = Path(__file__).parent
+
+dft_inlist_project = here / "inlist_project_unfilled"
+dft_inlist = here / "inlist"
+
+masses = np.geomspace(0.3, 20, 16)
+masses = np.append(masses, 1.0)
+print(masses)
+
+with dft_inlist_project.open('r') as dft:
+    dft_proj_txt = dft.read()
+    
+with dft_inlist.open('r') as dft:
+    dft_inl_txt = dft.read()
+
+names = here / "mass_list.txt"
+if names.exists():
+    names.unlink()
+    
+for mass in masses:
+    proj_txt = dft_proj_txt.replace("FILL_HERE",
+                                    f"{mass:0>5.2f}".replace('e', 'd'))
+    out_path = here / f"inlist_project_{mass:0>5.2f}"
+    with out_path.open('w') as out:
+        out.write(proj_txt)
+    out_inlist_path = here / f"inlist_{mass:0>5.2f}"
+    inl_txt = dft_inl_txt.replace("inlist_project",
+                                  f"inlist_project_{mass:0>5.2f}")
+    with out_inlist_path.open('w') as out:
+        
+        out.write(inl_txt)
+    
+    
+    with names.open('a') as fil:
+        fil.write(f"{mass:0>5.2f}\n")
+        
---
+++ src/run.f90
@@ -0,0 +1,34 @@
+program run
+use run_star_support, only: do_read_star_job
+use run_star, only: do_run_star
+
+implicit none
+
+integer :: ierr
+character (len=32) :: inlist_fname, mass_formatted, mass_list
+logical :: test_log
+real :: mass
+integer :: i
+
+ierr = 0
+
+open(2, file="mass_list.txt", status = "old")
+do i=1,17
+    read(2, *)  mass_formatted
+    ! mass = 0.3 + 1.313333 * (i - 1)
+    ! mass_formatted = "brew install gfortran"
+    ! write(mass_formatted, '(F 0 5.02)') mass
+    ! if (mass_formatted(1:1) == " ") then
+        ! mass_formatted(1:1) = "0"
+    ! else
+    ! end if
+    inlist_fname = "inlist_"//mass_formatted
+
+    print *, inlist_fname
+    call do_read_star_job(inlist_fname, ierr)
+    if (ierr == 0) then
+        call do_run_star(inlist_fname)
+    end if
+end do
+
+end program
